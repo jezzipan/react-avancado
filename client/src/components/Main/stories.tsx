f92fccf6762f723246318f52b3204a9dded326f7
jezzipan <jessica.leute@gmail.com>
2020-11-19 17:24:59 -0300

    trabalhando com  essential do storybook

--- client/src/components/Main/stories.tsx
+++ client/src/components/Main/stories.tsx
@@ -1,20 +1,20 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'title default',
+  description: 'description default'
+}
+
+export const Default: Story = (args) => <Main {...args} />
