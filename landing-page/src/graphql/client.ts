e049cce6e2a26a6797d567edd92f123afabf8b6e
jezzipan <jessica.leute@gmail.com>
2021-01-07 05:12:15 -0300

    client config

--- landing-page/src/graphql/client.ts
+++ landing-page/src/graphql/client.ts
@@ -1,8 +1,4 @@
 import { GraphQLClient } from 'graphql-request'
 
-const client = new GraphQLClient('http://127.0.0.1:1337/graphql')
-export default client
-import { GraphQLClient } from 'graphql-request'
-
 const client = new GraphQLClient('http://localhost:1337/graphql')
 export default client
